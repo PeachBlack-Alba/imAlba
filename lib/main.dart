aa1e0bdf9d8ccd986984276ffb8b873ef62bd873
Alba Torres Rodriguez <alba@jointhebase.co>
2020-09-24 11:42:50 +0200

    arranges for flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,23 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_web/material.dart';
+import 'package:personal_project/profile_page.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-    
-        primarySwatch: Colors.blue,
-       
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
+        primaryColorDark: Colors.black,
+        fontFamily: "GoogleSansRegular",
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-  
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-  
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      home: ProfilePage(),
     );
   }
 }
---
+++ web/main.dart
@@ -0,0 +1,8 @@
+
+// import '';
+// import 'package:personal_project/main.dart' as app;
+
+// main() async {
+//   await ui.webOnlyInitializePlatform();
+//   app.main();
+// }
